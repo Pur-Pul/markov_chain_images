1fd9bb62b24a8b83851a6be7d05685ef36f5346f
PurPul <axbergsamuel@gmail.com>
2022-09-14 20:58:17 +0300

    a chain can now be generated from a square table

--- src/basic_markov_chain.py
+++ src/basic_markov_chain.py
@@ -1,5 +1,6 @@
 class chain:
     def __init__(self, edges, n):
+        self.n = n
         self.adj = [None] * n
         self.lengths = [0] * n
         for i in range(n):
@@ -13,31 +14,56 @@ class chain:
         self.calculate_possibities()
 
     def calculate_possibities(self):
-        for a in range(n):
+        for a in range(self.n):
             a_len = len(self.adj[a])
             for j in range(a_len):
                 self.adj[a][j][1] /= self.lengths[a]
 
     def print(self):
-        for a in range(n):
+        for a in range(self.n):
             for j in range(len(self.adj[a])):
                 b = self.adj[a][j][0]
                 w = self.adj[a][j][1]
                 print("(" + str(a) + "," + str(b) + ")" + " : " + str(w))
 
-
-n = int(input())
-graph = []
-weights = {}
-for i in range(n):
-    key = input()
+def add_to_graph(key, weights):
     if key in weights:
         weights[key] += 1
     else: 
         weights[key] = 1
-for k in weights.keys():
-    a, b = k.split(" ")
-    graph.append((int(a), int(b), weights[k]))
+    return weights
 
-new_chain = chain(graph, n)
+def collect_edges(image):
+    graph = []
+    weights = {}
+    for i, row in enumerate(image):
+        for j, a in enumerate(row):
+            if i > 0:               #check color above
+                key = str(a)+" "+str(image[i-1][j])
+                weights = add_to_graph(key, weights)
+
+            if i < len(image)-1:    #check color below
+                key = str(a)+" "+str(image[i+1][j])
+                weights = add_to_graph(key, weights)
+            
+            if j > 0:               #check color left
+                key = str(a)+" "+str(image[i][j-1])
+                weights = add_to_graph(key, weights)
+            
+            if j < len(row) -1:     #check color right
+                key = str(a)+" "+str(image[i][j+1])
+                weights = add_to_graph(key, weights)
+    for k in weights.keys():
+        a, b = k.split(" ")
+        graph.append((int(a), int(b), weights[k]))
+    return graph
+        
+
+
+
+inp = [[0, 1, 2, 3], [1, 1, 0, 3], [0, 1, 3, 3], [2, 2, 2, 3]]
+
+graph = collect_edges(inp)
+
+new_chain = chain(graph, len(inp))
 new_chain.print()
