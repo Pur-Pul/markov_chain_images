30badf6b6cfdb647ad8a4d97bb63e7a9b57426b0
PurPul <axbergsamuel@gmail.com>
2022-09-15 13:27:49 +0300

    implemented BFS for image generation

--- src/basic_markov_chain.py
+++ src/basic_markov_chain.py
@@ -1,3 +1,31 @@
+import random
+
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Queue:
+    def __init__(self):
+        self.f_node = None
+        self.size = 0
+        self.end = None
+    def add(self, value):
+        if self.f_node == None:
+            self.f_node = Node(value)
+            self.end = self.f_node
+        else:
+            self.end.next = Node(value)
+            self.end = self.end.next
+        self.size += 1
+    def pop(self):
+        to_return = self.f_node.value
+        self.f_node = self.f_node.next
+        self.size -= 1
+        return to_return
+
 class chain:
     def __init__(self, edges, n):
         self.n = n
@@ -19,12 +47,50 @@ class chain:
             for j in range(a_len):
                 self.adj[a][j][1] /= self.lengths[a]
 
+    def get_neighbors(self, pos):
+        row_i, col_i = pos
+        neighbors = []
+        if row_i > 0:
+            neighbors.append((row_i-1, col_i))
+        if row_i < self.n-1:
+            neighbors.append((row_i+1, col_i))
+        if col_i > 0:
+            neighbors.append((row_i, col_i-1))
+        if col_i < self.n-1:
+            neighbors.append((row_i, col_i+1))
+        return neighbors
+
+    def generate_image(self):
+        image = [None] * self.n
+        for r in range(self.n):
+            image[r] = [-1]*self.n
+
+        start_pos = (random.randint(0, self.n-1), random.randint(0, self.n-1))
+        visited = {}
+        queue = Queue()
+                
+        #BFS
+        visited[start_pos] = True
+        queue.add(start_pos)
+        while(queue.size>0):
+            #print("queue_size: " + str(queue.size))
+            #print("pop()")
+            pos = queue.pop()
+            print(pos)
+
+            for neighbor in self.get_neighbors(pos):
+                if neighbor not in visited:
+                    visited[neighbor] = True
+                    #print("add: " + str(neighbor))
+                    queue.add(neighbor)
+
     def print(self):
         for a in range(self.n):
             for j in range(len(self.adj[a])):
-                b = self.adj[a][j][0]
-                w = self.adj[a][j][1]
+                b, w = self.adj[a][j]
                 print("(" + str(a) + "," + str(b) + ")" + " : " + str(w))
+
+
 
 def add_to_graph(key, weights):
     if key in weights:
@@ -67,3 +133,5 @@ graph = collect_edges(inp)
 
 new_chain = chain(graph, len(inp))
 new_chain.print()
+print()
+new_chain.generate_image()
