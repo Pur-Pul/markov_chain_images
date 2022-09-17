3805d8acc29a4f8c9b444ac40084a1faa6163fdb
PurPul <axbergsamuel@gmail.com>
2022-09-17 20:00:09 +0300

    Documentation

--- src/basic_markov_chain.py
+++ src/basic_markov_chain.py
@@ -4,15 +4,32 @@ import random
 
 class Node:
     def __init__(self, value):
+        """This the class for the nodes used in the queue.
+
+        Args:
+            value (_type_): The value of the node.
+            next (_type_): Points to the next node in the queue.
+        """
         self.value = value
         self.next = None
 
 class Queue:
     def __init__(self):
+        """This is the class for the queue used in BFS.
+        Args:
+            f_node Node: Points to the first node in the queue.
+            size int: Keeps track of the length of the queue.
+            end Node: Points to the last node in the queue
+        """
         self.f_node = None
         self.size = 0
         self.end = None
     def add(self, value):
+        """Assign the value to a new node and adds it to the end of the queue
+
+        Args:
+            value (_type_): The value of the node.
+        """
         if self.f_node == None:
             self.f_node = Node(value)
             self.end = self.f_node
@@ -21,6 +38,11 @@ class Queue:
             self.end = self.end.next
         self.size += 1
     def pop(self):
+        """Pops the first node from the queue and returns its value.
+
+        Returns:
+            value: The value of the popped node.
+        """
         to_return = self.f_node.value
         self.f_node = self.f_node.next
         self.size -= 1
@@ -28,6 +50,14 @@ class Queue:
 
 class Chain:
     def __init__(self, edges, n):
+        """This is the class for the markov chain.
+
+        Args:
+            edges (_type_): A list of tuples, that contain edges and their weight.
+            The weights are the frequency of the edges.
+            n int: The number of rows in the image and the max number colors allowed in the image. 
+            This will be changed later, so that more colors can be used in each image.
+        """
         self.n = n
         self.adj = [None] * n
         self.lengths = [0] * n
@@ -42,6 +72,8 @@ class Chain:
         self.calculate_possibities()
 
     def calculate_possibities(self):
+        """Redefines the weights of the edges to a percentage instead of frequency.
+        """
         for a in range(self.n):
             for b in range(self.n):
                 if self.lengths[a] == 0:
@@ -49,6 +81,14 @@ class Chain:
                 self.adj[a][b] /= self.lengths[a]
 
     def get_neighbors(self, pos):
+        """Checks neighbors of the given position and returns their coordinates.
+
+        Args:
+            pos tuple: The coordinates for the position to check.
+
+        Returns:
+            list: A list of the neighbors coordinates.
+        """
         row_i, col_i = pos
         neighbors = []
         if row_i > 0:
@@ -62,6 +102,15 @@ class Chain:
         return neighbors
 
     def pick_color(self, neighbors, image):
+        """This function decides the color to be assigned to the current position based on the colors of the neighbors.
+
+        Args:
+            neighbors list: A list of the neighbors coordinates.
+            image list: A 2d table representing the image to be generated.
+
+        Returns:
+            int: A color value.
+        """
         possibilities = {}
         counter = {}
         count = 0
@@ -100,6 +149,9 @@ class Chain:
         return rand_val
 
     def generate_image(self):
+        """This is the main function for generating an image based on the chain.
+        It uses BFS to traverse the image to be generated. The starting position is randomly picked.
+        """
         image = [None] * self.n
         for r in range(self.n):
             image[r] = [-1]*self.n
@@ -126,8 +178,13 @@ class Chain:
         self.print_image(image)
 
     def print_image(self, image):
-        for i, row in enumerate(image):
-            for j, a in enumerate(row):
+        """This function prints the image to the console.
+
+        Args:
+            image list: A 2d table representing the image to be printed.
+        """
+        for _, row in enumerate(image):
+            for _, a in enumerate(row):
                 print(a, end=", ")
             print()
 
@@ -140,6 +197,15 @@ class Chain:
 
 
 def add_to_graph(key, weights):
+    """This function makes sure each edge appears only once, and instead increments the weights if they appear multiple times.
+
+    Args:
+        key (_type_): The unique key for the current edge, to be used in a dictionary.
+        weights (_type_): A dictionary containing the weights of each edge.
+
+    Returns:
+        _type_: _description_
+    """
     if key in weights:
         weights[key] += 1
     else: 
@@ -147,6 +213,13 @@ def add_to_graph(key, weights):
     return weights
 
 def collect_edges(image):
+    """Collects the edges to be entered into the markov chain.
+
+    Args:
+        image list: A 2d table that represent the image to be analyzed.
+    Returns:
+        _type_: A list of edges.
+    """
     graph = []
     weights = {}
     for i, row in enumerate(image):
